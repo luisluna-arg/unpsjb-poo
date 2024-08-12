892a89f70a02f274ff1bdb1467066cc5032af56b
Luis Luna <luismluna87@gmail.com>
2024-08-12 02:01:35 -0300

    Added examples

---
+++ eclipse/Example2/src/main/java/org/example/example2/Example2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.example.example2;
+
+/**
+ *
+ * @author luis
+ */
+public class Example2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
