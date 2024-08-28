c8764d8225ec6b74f27bfbeff81b481076f78a00
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2024-08-28 11:17:02 -0300

    Create no_docente.py

---
+++ python/p3_ejercicio10/personas/no_docente.py
@@ -0,0 +1,10 @@
+from random import random
+from models.jornada import Jornada
+from personas.persona import Persona
+
+class NoDocente(Persona):
+
+    def __init__(self):
+        super().__init__()    
+        self.__jornada = Jornada.get_jornada()
+        self.__cantidad_horas = random.random() > self.__jornada
