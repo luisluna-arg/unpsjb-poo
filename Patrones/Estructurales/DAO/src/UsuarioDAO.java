e3b79a096c3e228d4545c971c77280b3c7091403
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2024-10-14 15:35:32 -0300

    Reordenado y agregados Patrones en Python

--- Patrones/Estructurales/DAO/src/UsuarioDAO.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface UsuarioDAO {
-    Usuario obtenerUsuarioPorId(int id);
-    List<Usuario> obtenerTodosLosUsuarios();
-    void insertarUsuario(Usuario usuario);
-    void actualizarUsuario(Usuario usuario);
-    void eliminarUsuario(int id);
-}
